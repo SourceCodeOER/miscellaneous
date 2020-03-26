5a4a969315561d7c01b173513f34cbf9750b3242
Jacques Yakoub <jacques.yakoub@gmail.com>
2020-03-26 20:52:19 +0100

    feat(db) : add some tags

--- sourcecode-postgres/scripts/demo-db_with_extra.sql
+++ sourcecode-postgres/scripts/demo-db_with_extra.sql
@@ -2,3 +2,12 @@
 
 UPDATE exercises_library."Exercises"
 SET state = 'VALIDATED';
+
+-- Creates a Tag with "course" category with text "validation mémoire"
+COPY exercises_library."Tags" (id, text, "createdAt", "updatedAt", version, category_id, state) FROM stdin;
+178	validation mémoire	2020-03-04 11:48:58.716+00	2020-03-04 11:48:58.716+00	0	2	VALIDATED
+179	StudentJob	2020-03-04 11:48:58.716+00	2020-03-04 11:48:58.716+00	0	3	VALIDATED
+180	match	2020-03-04 11:48:58.716+00	2020-03-04 11:48:58.716+00	0	2	DEPRECATED
+181	example	2020-03-04 11:48:58.716+00	2020-03-04 11:48:58.716+00	0	2	DEPRECATED
+\.
+
