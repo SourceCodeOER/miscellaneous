e4ba8f700fa85fa93dc1e9050e58e3ab4db5ad5a
Jacques Yakoub <jacques.yakoub@gmail.com>
2020-04-27 21:22:26 +0200

    feat(db) : Add the exercises for validation
    Backup in case we have a problem tomorrow XD

--- sourcecode-postgres/scripts/demo-db_with_extra.sql
+++ sourcecode-postgres/scripts/demo-db_with_extra.sql
@@ -57,3 +57,41 @@ FROM unnest(
         ''
     )
 ) a;
+
+-- Add the common exercises used for validation
+COPY exercises_library."Exercises" (id, title, description, "createdAt", "updatedAt", version, user_id, state, file, url) FROM stdin;
+224	Decision Making in Java - if	<p>if statement is the most simple decision making statement. It is used to decide whether a certain statement or block of statements will be executed or not i.e if a certain condition is true then a block of statement is executed otherwise not.</p><p>Syntax:</p><p>if(condition) {</p><p>   // Statements to execute if</p><p>   // condition is true</p><p>}</p><p>Here, condition after evaluation will be either true or false. if statement accepts boolean values – if the value is true then it will execute the block of statements under it.</p><p>If we do not provide the curly braces { and } after if( condition ) then by default if statement will consider the immediate one statement to be inside its block.</p><p>Example :</p><p>// Java program to illustrate If statement </p><p>class IfDemo </p><p>{ </p><p>    public static void main(String args[]) </p><p>    { </p><p>        int i = 10; </p><p>  </p><p>        if (i &gt; 15) </p><p>            System.out.println("10 is less than 15"); </p><p>  </p><p>        // This statement will be executed </p><p>        // as if considers one statement by default </p><p>        System.out.println("I am Not in if"); </p><p>    } </p><p>} </p><p>Output:</p><p>I am Not in if</p>	2020-04-26 07:03:48.24+00	2020-04-26 07:03:48.242+00	0	1	PENDING	\N	\N
+225	Decision Making in Java - if-else	<p>The if statement alone tells us that if a condition is true it will execute a block of statements and if the condition is false it won’t. But what if we want to do something else if the condition is false. Here comes the else statement. We can use the else statement with if statement to execute a block of code when the condition is false.</p><p>Syntax:</p><p>if (condition)</p><p>{</p><p>    // Executes this block if</p><p>    // condition is true</p><p>}</p><p>else</p><p>{</p><p>    // Executes this block if</p><p>    // condition is false</p><p>}</p><p>Example:</p><p>// Java program to illustrate if-else statement </p><p>class IfElseDemo </p><p>{ </p><p>    public static void main(String args[]) </p><p>    { </p><p>        int i = 10; </p><p>  </p><p>        if (i &lt; 15) </p><p>            System.out.println("i is smaller than 15"); </p><p>        else</p><p>            System.out.println("i is greater than 15"); </p><p>    } </p><p>} </p><p>Output:</p><p>i is smaller than 15</p>	2020-04-26 07:05:34.263+00	2020-04-26 07:05:34.264+00	0	1	PENDING	\N	\N
+226	Decision Making in Java - nested-if	<p>A nested if is an if statement that is the target of another if or else. Nested if statements means an if statement inside an if statement. Yes, java allows us to nest if statements within if statements. i.e, we can place an if statement inside another if statement.</p><p>Syntax:</p><p>if (condition1) </p><p>{</p><p>   // Executes when condition1 is true</p><p>   if (condition2) </p><p>   {</p><p>      // Executes when condition2 is true</p><p>   }</p><p>}</p><p>Example:</p><p>// Java program to illustrate nested-if statement </p><p>class NestedIfDemo </p><p>{ </p><p>    public static void main(String args[]) </p><p>    { </p><p>        int i = 10; </p><p>  </p><p>        if (i == 10) </p><p>        { </p><p>            // First if statement </p><p>            if (i &lt; 15) </p><p>                System.out.println("i is smaller than 15"); </p><p>  </p><p>            // Nested - if statement </p><p>            // Will only be executed if statement above </p><p>            // it is true </p><p>            if (i &lt; 12) </p><p>                System.out.println("i is smaller than 12 too"); </p><p>            else</p><p>                System.out.println("i is greater than 15"); </p><p>        } </p><p>    } </p><p>} </p><p>Output:</p><p>i is smaller than 15</p><p>i is smaller than 12 too</p>	2020-04-26 07:06:38.67+00	2020-04-26 07:06:38.67+00	0	1	PENDING	\N	\N
+227	Decision Making in Java - if-else-if ladder	<p>Here, a user can decide among multiple options.The if statements are executed from the top down. As soon as one of the conditions controlling the if is true, the statement associated with that if is executed, and the rest of the ladder is bypassed. If none of the conditions is true, then the final else statement will be executed.</p><p>if (condition)</p><p>    statement;</p><p>else if (condition)</p><p>    statement;</p><p>//...</p><p>//...</p><p>else</p><p>    statement;</p><p>Example:</p><p>// Java program to illustrate if-else-if ladder </p><p>class ifelseifDemo </p><p>{ </p><p>    public static void main(String args[]) </p><p>    { </p><p>        int i = 20; </p><p>  </p><p>        if (i == 10) </p><p>            System.out.println("i is 10"); </p><p>        else if (i == 15) </p><p>            System.out.println("i is 15"); </p><p>        else if (i == 20) </p><p>            System.out.println("i is 20"); </p><p>        else</p><p>            System.out.println("i is not present"); </p><p>    } </p><p>} </p><p>Output:</p><p>i is 20</p>	2020-04-26 07:07:26.682+00	2020-04-26 07:07:26.683+00	0	1	PENDING	\N	\N
+\.
+
+COPY exercises_library."Exercises_Metrics" (id, vote_count, avg_vote_score, tags_ids, exercise_id) FROM stdin;
+224	0	0.00	{178,133,180,179,49}	224
+225	0	0.00	{49,178,179,181,133}	225
+226	0	0.00	{179,133,178,49,182}	226
+227	0	0.00	{183,178,179,133,49}	227
+\.
+
+COPY exercises_library."Exercises_Tags" (exercise_id, tag_id) FROM stdin;
+224	178
+224	133
+224	180
+224	179
+224	49
+225	49
+225	178
+225	179
+225	181
+225	133
+226	179
+226	133
+226	178
+226	49
+226	182
+227	183
+227	178
+227	179
+227	133
+227	49
+\.
